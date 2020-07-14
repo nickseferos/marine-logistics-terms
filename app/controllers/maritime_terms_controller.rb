e1e763107298d975933990a72ad0c50621b93f16
Nick Seferos <nick.seferos@gmail.com>
2020-07-14 11:17:23 -0700

    Complete MVP

--- app/controllers/maritime_terms_controller.rb
+++ app/controllers/maritime_terms_controller.rb
@@ -13,53 +13,53 @@ class MaritimeTermsController < ApplicationController
   end
 
   # GET /maritime_terms/new
-  def new
-    @maritime_term = MaritimeTerm.new
-  end
+  # def new
+  #   @maritime_term = MaritimeTerm.new
+  # end
 
   # GET /maritime_terms/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /maritime_terms
   # POST /maritime_terms.json
-  def create
-    @maritime_term = MaritimeTerm.new(maritime_term_params)
-
-    respond_to do |format|
-      if @maritime_term.save
-        format.html { redirect_to @maritime_term, notice: 'Maritime term was successfully created.' }
-        format.json { render :show, status: :created, location: @maritime_term }
-      else
-        format.html { render :new }
-        format.json { render json: @maritime_term.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def create
+  #   @maritime_term = MaritimeTerm.new(maritime_term_params)
+  #
+  #   respond_to do |format|
+  #     if @maritime_term.save
+  #       format.html { redirect_to @maritime_term, notice: 'Maritime term was successfully created.' }
+  #       format.json { render :show, status: :created, location: @maritime_term }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @maritime_term.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /maritime_terms/1
   # PATCH/PUT /maritime_terms/1.json
-  def update
-    respond_to do |format|
-      if @maritime_term.update(maritime_term_params)
-        format.html { redirect_to @maritime_term, notice: 'Maritime term was successfully updated.' }
-        format.json { render :show, status: :ok, location: @maritime_term }
-      else
-        format.html { render :edit }
-        format.json { render json: @maritime_term.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @maritime_term.update(maritime_term_params)
+  #       format.html { redirect_to @maritime_term, notice: 'Maritime term was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @maritime_term }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @maritime_term.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /maritime_terms/1
   # DELETE /maritime_terms/1.json
-  def destroy
-    @maritime_term.destroy
-    respond_to do |format|
-      format.html { redirect_to maritime_terms_url, notice: 'Maritime term was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @maritime_term.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to maritime_terms_url, notice: 'Maritime term was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/views/maritime_terms/_maritime_term.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! maritime_term, :id, :term, :explination, :created_at, :updated_at
-json.url maritime_term_url(maritime_term, format: :json)
